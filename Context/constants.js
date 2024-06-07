bfd763fb070c9c81a2357282ea75968d6e158fb6
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-06-07 07:38:04 +0530

    work completed

---
+++ Context/constants.js
@@ -0,0 +1,13 @@
+//ERC20Generator = 0xDC3A2E74Dfc370E4129d143661D9fDa9D3080537
+//LookUpContract = 0x5f0dBeF664d9d6D047FeB500E285492eAe8DE644
+import ERC20Generator from "./ERC20Generator.json";
+import LookUpContract from "./LookUpContract.json";
+
+export const ERC20Generator_ABI = ERC20Generator.abi;
+export const ERC20Generator_BYTECODE = ERC20Generator.bytecode;
+export const ERC20Generator_ADDRESS =
+  "0xb2638CE506d71F5038661FeE409C4A00fA12F68d";
+
+export const LookUpContract_ABI = LookUpContract.abi;
+export const LookUpContract_ADDRESS =
+  "0x273B3bcE9e1957b7897c26d86fD589b306AbE796";
---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc-amoy.polygon.technology";
+const NEXT_PUBLIC_PRIVATE_KEY = "3b56a36b0056e5c5200e853d52b48898438a63c1fee653d13c4fa683015c6307";
+module.exports = {
+  solidity: "0.8.0",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+};
